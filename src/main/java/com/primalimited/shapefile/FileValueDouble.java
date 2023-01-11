456cc7f0849d404ecaf3d94c325af0d9cf51ede6
Jim Newpower <jimnewpower@gmail.com>
2023-01-11 09:11:53 -0700

    Add record constructors to validate arguments

--- src/main/java/com/primalimited/shapefile/FileValueDouble.java
+++ src/main/java/com/primalimited/shapefile/FileValueDouble.java
@@ -1,6 +1,7 @@
 package com.primalimited.shapefile;
 
 import java.nio.ByteOrder;
+import java.util.Objects;
 
 public record FileValueDouble(
         long bytePosition,
@@ -8,4 +9,12 @@ public record FileValueDouble(
         double value,
         ByteOrder byteOrder
 ) {
+    public FileValueDouble {
+        if (bytePosition < 0)
+            throw new IllegalArgumentException(String.format("Specified bytePosition %d must be >= 0.", bytePosition));
+        Objects.requireNonNull(fieldDescription, "field description");
+        if (Double.isInfinite(value) || Double.isNaN(value))
+            throw new IllegalArgumentException(String.format("Specified value %g is invalid.", value));
+        Objects.requireNonNull(byteOrder, "byte order");
+    }
 }
--- src/main/java/com/primalimited/shapefile/FileValueInt.java
+++ src/main/java/com/primalimited/shapefile/FileValueInt.java
@@ -1,6 +1,7 @@
 package com.primalimited.shapefile;
 
 import java.nio.ByteOrder;
+import java.util.Objects;
 
 public record FileValueInt(
         long bytePosition,
@@ -8,4 +9,10 @@ public record FileValueInt(
         int value,
         ByteOrder byteOrder
 ) {
+    public FileValueInt {
+        if (bytePosition < 0)
+            throw new IllegalArgumentException(String.format("Specified bytePosition %d must be >= 0.", bytePosition));
+        Objects.requireNonNull(fieldDescription, "field description");
+        Objects.requireNonNull(byteOrder, "byte order");
+    }
 }
