1e4543b702dea1773c48fbc3be75e85efdfb35aa
Jim Newpower <jimnewpower@gmail.com>
2023-01-11 11:03:16 -0700

    Make defensive copies of list arguments in Polyline.

--- src/main/java/com/primalimited/shapefile/Polyline.java
+++ src/main/java/com/primalimited/shapefile/Polyline.java
@@ -1,8 +1,30 @@
 package com.primalimited.shapefile;
 
 import java.util.List;
+import java.util.Objects;
 
 public record Polyline(BoundingBox boundingBox, int numParts, List<Integer> partStartIndices, List<Point> points) {
+    public Polyline {
+        Objects.requireNonNull(boundingBox, "bounding box");
+
+        if (numParts < 1)
+            throw new IllegalArgumentException(String.format("numParts must be at least 1 (was %d)", numParts));
+
+        Objects.requireNonNull(partStartIndices, "part start indices");
+        if (partStartIndices.isEmpty())
+            throw new IllegalArgumentException("partStartIndices is empty");
+
+        // Make defensive copy of list
+        partStartIndices = List.copyOf(partStartIndices);
+
+        Objects.requireNonNull(points, "points");
+        if (points.isEmpty())
+            throw new IllegalArgumentException("points is empty");
+
+        // Make defensive copy of list
+        points = List.copyOf(points);
+    }
+
     public int numPoints() {
         return points.size();
     }
--- src/test/java/com/primalimited/shapefile/PolylineTest.java
+++ src/test/java/com/primalimited/shapefile/PolylineTest.java
@@ -2,6 +2,7 @@ package com.primalimited.shapefile;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,10 +17,18 @@ class PolylineTest {
                 new Point(10, 10),
                 new Point(10, 0)
         };
+        ArrayList<Point> list = new ArrayList<>(List.of(points));
         BoundingBox boundingBox = new BoundingBox(Bounds.of(0, 10), Bounds.of(0, 10));
-        Polyline polyline = new Polyline(boundingBox, 1, List.of(1), List.of(points));
+        Polyline polyline = new Polyline(boundingBox, 1, List.of(1), list);
         assertEquals(boundingBox, polyline.boundingBox());
         assertEquals(1, polyline.numParts());
         assertEquals(points.length, polyline.numPoints());
+
+        // Ensure that adding a point to our list after creating the polyline doesn't modify the
+        // underlying list in the polyline itself (tests that the polyline record made a defensive
+        // copy of the points list).
+        list.add(new Point(50, 50));
+        assertEquals(points.length, polyline.numPoints());
+
     }
 }
