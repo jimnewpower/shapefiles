a544c4c13ace87ac4a00efe48346b388bcf1c7db
Jim Newpower <jimnewpower@gmail.com>
2023-01-11 09:36:22 -0700

    Add predefined FILE_HEADER bytes definition

--- src/main/java/com/primalimited/shapefile/ByteValue.java
+++ src/main/java/com/primalimited/shapefile/ByteValue.java
@@ -1,8 +1,12 @@
 package com.primalimited.shapefile;
 
 public record ByteValue(long bytes) {
+    // Integer values are always 4 bytes.
     public static final ByteValue BYTE_VALUE_INT = new ByteValue(4);
+    // Double values are always 8 bytes.
     public static final ByteValue BYTE_VALUE_DOUBLE = new ByteValue(8);
+    // File header for the main (shp) and index (shx) files is 100 bytes.
+    public static final ByteValue FILE_HEADER = new ByteValue(100);
 
     public ByteValue {
         if (bytes < 0)
--- src/test/java/com/primalimited/shapefile/ByteValueTest.java
+++ src/test/java/com/primalimited/shapefile/ByteValueTest.java
@@ -17,4 +17,17 @@ class ByteValueTest {
         assertEquals(4, new ByteValue(4).bytes());
         assertEquals(2, new ByteValue(4).to16BitWords());
     }
+
+    @Test
+    public void testPredefined() {
+        assertEquals(4, ByteValue.BYTE_VALUE_INT.bytes());
+        assertEquals(2, ByteValue.BYTE_VALUE_INT.to16BitWords());
+
+        assertEquals(8, ByteValue.BYTE_VALUE_DOUBLE.bytes());
+        assertEquals(4, ByteValue.BYTE_VALUE_DOUBLE.to16BitWords());
+
+        assertEquals(100, ByteValue.FILE_HEADER.bytes());
+        assertEquals(50, ByteValue.FILE_HEADER.to16BitWords());
+
+    }
 }
