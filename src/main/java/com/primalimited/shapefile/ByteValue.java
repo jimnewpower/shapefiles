7e1b843333d4783d91d62b901d3c5c0f905a3fbc
Jim Newpower <jimnewpower@gmail.com>
2023-01-12 09:58:27 -0700

    Add javadoc

--- src/main/java/com/primalimited/shapefile/ByteValue.java
+++ src/main/java/com/primalimited/shapefile/ByteValue.java
@@ -2,6 +2,11 @@ package com.primalimited.shapefile;
 
 import java.util.Objects;
 
+/**
+ * A byte value represents the number of bytes, which can be used for
+ * sizing of buffers, or positions in files. ESRI Shapefiles use 16-bit
+ * words for sizing, so this provides a conversion function.
+ */
 public record ByteValue(long bytes) {
     // Integer values are always 4 bytes.
     public static final ByteValue BYTE_VALUE_INT = new ByteValue(4);
@@ -17,17 +22,30 @@ public record ByteValue(long bytes) {
             throw new IllegalArgumentException(String.format("Specified bytes %d must be >= 0.", bytes));
     }
 
+    /**
+     * ESRI Shapefiles use 16-bit words for sizing, which is nBytes / 2.
+     * @return nBytes / 2.
+     */
     public long to16BitWords() {
         return bytes / 2;
     }
 
+    /**
+     * Create new ByteValue instance, adding nBytes to the existing value.
+     * @param nBytes number of bytes to add.
+     * @return new ByteValue instance, adding nBytes to the existing value.
+     */
     public ByteValue add(int nBytes) {
         return new ByteValue(bytes() + nBytes);
     }
 
+    /**
+     * Create new ByteValue instance, adding byteValue nBytes to the existing value.
+     * @param byteValue number of bytes to add.
+     * @return new ByteValue instance, adding nBytes to the existing value.
+     */
     public ByteValue add(ByteValue byteValue) {
         Objects.requireNonNull(byteValue, "byte value");
         return new ByteValue(bytes() + (int)byteValue.bytes());
     }
-
 }
