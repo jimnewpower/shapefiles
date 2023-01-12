ea90d22b0fea9dc3425402e470aa8fd75730fd66
Jim Newpower <jimnewpower@gmail.com>
2023-01-12 10:26:29 -0700

    Add javadoc, clean code

--- src/main/java/com/primalimited/shapefile/Writer.java
+++ src/main/java/com/primalimited/shapefile/Writer.java
@@ -13,8 +13,20 @@ import java.util.Objects;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
+/**
+ * Shapefile writer interface.
+ */
 public interface Writer {
 
+    /**
+     * Write a shapefile.
+     * @param directory directory in which the files will be created.
+     * @param baseFilename base filename for all shapefiles.
+     * @param header header information.
+     * @param dataset the shapefile dataset.
+     * @return path to main (.shp) file.
+     * @throws IOException
+     */
     default Path write(Path directory, String baseFilename, Header header, Dataset dataset) throws IOException {
         checkArgs(directory, baseFilename, header, dataset);
 
@@ -34,18 +46,40 @@ public interface Writer {
         return mainPath;
     }
 
+    /**
+     * Write the main (.shp) file.
+     * @param header header information.
+     * @param dataset the shapefile record data.
+     * @param mainFileOutputStream the output stream.
+     * @throws IOException
+     */
     void writeMainFile(
         Header header,
         Dataset dataset,
         BufferedOutputStream mainFileOutputStream
     ) throws IOException;
 
+    /**
+     * Write the index (.shx) file.
+     * @param header header information.
+     * @param dataset the shapefile record data.
+     * @param indexFileOutputStream the output stream.
+     * @throws IOException
+     */
     void writeIndexFile(
         Header header,
         Dataset dataset,
         BufferedOutputStream indexFileOutputStream
     ) throws IOException;
 
+    /**
+     * Check arguments supplied to write(), throwing IllegalArgumentExceptions as necessary.
+     *
+     * @param directory directory in which the files will be created.
+     * @param baseFilename base filename for all shapefiles.
+     * @param header header information.
+     * @param dataset the shapefile dataset.
+     */
     default void checkArgs(Path directory, String baseFilename, Header header, Dataset dataset) {
         Objects.requireNonNull(directory, "directory");
         if (!directory.toFile().isDirectory())
@@ -65,6 +99,12 @@ public interface Writer {
             throw new IllegalArgumentException("Dataset must contain non-null, non-empty points list.");
     }
 
+    /**
+     * Given a directory and a base filename, create the main (.shp) shapefile path.
+     * @param directory directory in which the files will be created.
+     * @param baseFilename base filename for all shapefiles.
+     * @return path object representing the main (.shp) shapefile.
+     */
     default Path createMainPath(Path directory, String baseFilename) {
         // Strip file extension if there is one.
         String filename = Filenames.removeFileExtension(baseFilename);
@@ -72,6 +112,12 @@ public interface Writer {
         return Paths.get(directory.toString(), mainFilename);
     }
 
+    /**
+     * Given a directory and a base filename, create the index (.shx) shapefile path.
+     * @param directory directory in which the files will be created.
+     * @param baseFilename base filename for all shapefiles.
+     * @return path object representing the index (.shx) shapefile.
+     */
     default Path createIndexPath(Path directory, String baseFilename) {
         // Strip file extension if there is one.
         String filename = Filenames.removeFileExtension(baseFilename);
@@ -79,6 +125,12 @@ public interface Writer {
         return Paths.get(directory.toString(), indexFilename);
     }
 
+    /**
+     * Write the shapefile header.
+     * @param header shapefile 100-byte header.
+     * @param stream the output stream.
+     * @throws IOException
+     */
     default void writeHeader(Header header, BufferedOutputStream stream) throws IOException {
         ByteBuffer headerBufferBigEndian = ByteBuffer.allocate(Header.BIG_ENDIAN_N_BYTES).order(ByteOrder.BIG_ENDIAN);
         ByteBuffer headerBufferLittleEndian = ByteBuffer.allocate(Header.LITTLE_ENDIAN_N_BYTES).order(ByteOrder.LITTLE_ENDIAN);
--- src/main/java/com/primalimited/shapefile/WriterPoint.java
+++ src/main/java/com/primalimited/shapefile/WriterPoint.java
@@ -6,6 +6,9 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.List;
 
+/**
+ * Shapefile writer for Point record data.
+ */
 class WriterPoint implements Writer {
 
     @Override
@@ -23,8 +26,12 @@ class WriterPoint implements Writer {
         contentLength = contentLength.add(ByteValue.BYTE_VALUE_DOUBLE);//x
         contentLength = contentLength.add(ByteValue.BYTE_VALUE_DOUBLE);//y
 
-        ByteBuffer recordHeaderBuffer = ByteBuffer.allocate((int)ByteValue.RECORD_HEADER.bytes()).order(ByteOrder.BIG_ENDIAN);
-        ByteBuffer recordBuffer = ByteBuffer.allocate((int)contentLength.bytes()).order(ByteOrder.LITTLE_ENDIAN);
+        ByteBuffer recordHeaderBuffer = ByteBuffer
+                .allocate((int)ByteValue.RECORD_HEADER.bytes())
+                .order(ByteOrder.BIG_ENDIAN);
+        ByteBuffer recordBuffer = ByteBuffer
+                .allocate((int)contentLength.bytes())
+                .order(ByteOrder.LITTLE_ENDIAN);
 
         int recordIndex = 1;
 
@@ -58,7 +65,9 @@ class WriterPoint implements Writer {
         contentLength = contentLength.add(ByteValue.BYTE_VALUE_DOUBLE);//x
         contentLength = contentLength.add(ByteValue.BYTE_VALUE_DOUBLE);//y
 
-        ByteBuffer indexRecordBuffer = ByteBuffer.allocate((int)ByteValue.RECORD_HEADER.bytes()).order(ByteOrder.BIG_ENDIAN);
+        ByteBuffer indexRecordBuffer = ByteBuffer
+                .allocate((int)ByteValue.RECORD_HEADER.bytes())
+                .order(ByteOrder.BIG_ENDIAN);
 
         int offset = 0;
 
