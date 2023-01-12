aa447d4f14788c91ecfac10521550746e3b9693f
Jim Newpower <jimnewpower@gmail.com>
2023-01-12 09:54:40 -0700

    Add javadoc

--- src/main/java/com/primalimited/shapefile/BoundingBox.java
+++ src/main/java/com/primalimited/shapefile/BoundingBox.java
@@ -2,6 +2,9 @@ package com.primalimited.shapefile;
 
 import java.util.Objects;
 
+/**
+ * Bounding Box record, for storing a spatial bounding rectangle.
+ */
 public record BoundingBox(Bounds xBounds, Bounds yBounds) {
     public BoundingBox {
         Objects.requireNonNull(xBounds, "x bounds");
@@ -12,6 +15,12 @@ public record BoundingBox(Bounds xBounds, Bounds yBounds) {
             throw new IllegalArgumentException(String.format("yBounds argument is invalid (%g - %g).", yBounds.getMin(), yBounds.getMax()));
     }
 
+    /**
+     * Return the bounding box values as an array, in the order in which
+     * the shapefile expects them (Xmin, Ymin, Xmax, Ymax).
+     * @return the bounding box values as an array, in the order in which
+     * the shapefile expects them (Xmin, Ymin, Xmax, Ymax).
+     */
     public double[] getShapefileBoundingBoxValues() {
         // The Bounding Box is stored in the order Xmin, Ymin, Xmax, Ymax
         return new double[] { xBounds.getMin(), yBounds.getMin(), xBounds.getMax(), yBounds.getMax() };
