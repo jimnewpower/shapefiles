75e73e0b379064b10d5a03d6a980d1b3db4a988d
Jim Newpower <jimnewpower@gmail.com>
2023-01-12 10:02:42 -0700

    Add javadoc

--- src/main/java/com/primalimited/shapefile/Dataset.java
+++ src/main/java/com/primalimited/shapefile/Dataset.java
@@ -3,6 +3,9 @@ package com.primalimited.shapefile;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Used for generic writer class to get data based on shape type.
+ */
 public interface Dataset {
     ShapeType shapeType();
 
