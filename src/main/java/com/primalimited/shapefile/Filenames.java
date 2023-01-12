30f9da50b5897153f3733f4614094f3cc6c9181e
Jim Newpower <jimnewpower@gmail.com>
2023-01-12 10:04:30 -0700

    Add javadoc

--- src/main/java/com/primalimited/shapefile/Filenames.java
+++ src/main/java/com/primalimited/shapefile/Filenames.java
@@ -1,8 +1,16 @@
 package com.primalimited.shapefile;
 
+/**
+ * Filename utility functions.
+ */
 public class Filenames {
     private static final String EXTENSION_PATTERN = "(?<!^)[.]" + "[^.]*$";
 
+    /**
+     * Remove the extension from a filename.
+     * @param filename filename argument.
+     * @return filename string without its extension.
+     */
     public static String removeFileExtension(String filename) {
         if (filename == null || filename.isEmpty())
             return filename;
@@ -10,6 +18,12 @@ public class Filenames {
         return filename.replaceAll(EXTENSION_PATTERN, "");
     }
 
+    /**
+     * Add extension to a filename.
+     * @param baseFilename base filename argument.
+     * @param extension extension to add.
+     * @return string with extension added to the filename.
+     */
     public static String addFileExtension(String baseFilename, String extension) {
         String ext = extension.startsWith(".") ? extension : "." + extension;
         return baseFilename + ext;
